a10371e5addf7092601d7f9b215e51562ed6887b
Jean-Philippe Bidegain <jean-philippe@kaizen-developments.com>
2013-05-16 16:34:12 +0200

    Added a new sitemap name

--- classes/tests/Sitemap.php
+++ classes/tests/Sitemap.php
@@ -2,7 +2,7 @@
 
 class Sitemap extends FileExistsTest {
 	public $route = 'sitemap/';
-	public $file = array('sitemap.xml', 'index_sitemap.xml');
+	public $file = array('sitemap.xml', 'index_sitemap.xml', 'sitemap_index.xml');
 	public $name = 'sitemap.xml';
 	public $description = 'Checks if the sitemap.xml file exists';
 	public $successMessage = 'OK: File exists';
